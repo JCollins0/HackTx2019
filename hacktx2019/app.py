fa3102bc2757c6188bed3a3c184ac904f1995350
JCollins0 <jrc.collins01@gmail.com>
2019-11-02 18:45:11 -0500

    Add more routes to flask app

--- hacktx2019/app.py
+++ hacktx2019/app.py
@@ -1,13 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
-import pymongo
 
-@app.route('/')
-def hello_world():
 
+@app.route('/login')
+def login():
+
+    import pymongo
     client = pymongo.MongoClient("mongodb+srv://hacktx-user:hacktx-user@hacktx-mb5vn.azure.mongodb.net/test?retryWrites=true&w=majority")
     print(client.list_database_names())
 
+    return "Hello Login!"
+
+@app.route('/rec')
+def rec():
+    return "Hellow Rec!"
+
+@app.route('/find')
+def find():
+    return "Hello Find!"
+
+@app.route('/')
+def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
